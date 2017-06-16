57cc92707a08e7e8c9c35117c977ab8240121e14
JakiroBestSupport <leonkhvatskin@gmail.com>
2017-06-16 14:59:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/dont_say_the_l_word/RulesActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.dont_say_the_l_word;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RulesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
